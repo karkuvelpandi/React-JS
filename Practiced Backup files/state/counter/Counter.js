5128434edca9e588e3495238f05d1de155363282
karkuvelpandi <karkuvelpandi007@gmail.com>
2022-09-22 05:57:18 +0530

    counter

--- Practiced Backup files/state/counter/Counter.js
+++ Practiced Backup files/state/counter/Counter.js
@@ -3,21 +3,32 @@ import React from "react"
 class Counter extends React.Component {
  /*    counter = 1 */
  state={
-    counter:1
+    counter:1,
+    msg :"e"
  }
     updateCounter= ()=> {
        this.setState({counter:this.state.counter+1})
     };
-    decrementHandler=(event)=>{
+    stopHandler=(event)=>{
+    
+     /*    this.setState({counter:0}) */
+        this.setState({counter:0,msg:"a"})
+
+         
+    };
+    decrementHandler=()=>{
      this.setState({counter:this.state.counter-1})
-   /*   if(this.state.counter===0){
-          event.addeventListe
-     } */
+     if( this.state.counter<=0){
+     
+        return this.stopHandler()
+     }
     };
     render() {
         console.log('first ');
         return <React.Fragment>
             <h1>Counter value={this.state.counter}</h1>
+            <span>{this.msg}</span> 
+            <br/><br/>
             <button onClick={this.updateCounter}>Increment counter</button>
             <button onClick={this.decrementHandler}>Decrement Counter</button>
         </React.Fragment>
