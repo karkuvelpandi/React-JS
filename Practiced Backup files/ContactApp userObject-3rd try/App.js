e0fb50cd447df0caecc7f9a41b7ef78f5b013854
karkuvelpandi <karkuvelpandi007@gmail.com>
2022-10-08 05:24:34 +0530

    3rd try

---
+++ Practiced Backup files/ContactApp userObject-3rd try/App.js
@@ -0,0 +1,25 @@
+import React from "react"
+import UseState from './UseState/UseState'
+import Navbar from './Navbar/Navbar'
+import Counter from './Counter/Counter'
+import Clock from './Clock/Clock'
+import ContactApp from './ContactApp/ContactApp'
+import UserApp from "./UserApp/UserApp"
+import {BrowserRouter as Router, Routes,Route}from 'react-router-dom'
+let App =()=>{
+   return <>
+   <Router>
+      <Navbar/>
+      <Routes>
+         <Route path='/UseState' element={<UseState/>}/>
+         <Route path='/Counter' element={<Counter/>}/>
+         <Route path='/Clock' element={<Clock/>}/>
+         <Route exact path='/ContactApp' element={<ContactApp/>}/>
+         <Route exact path='/UserApp' element={<UserApp/>}/>
+      </Routes>
+   </Router>
+   
+   
+   </>
+};
+export default App
