4c6fc6a0f8c6e4f2d589624e934a107c6c54cb90
karkuvelpandi <karkuvelpandi007@gmail.com>
2022-10-10 09:35:23 +0530

    update

--- Practiced Backup files/ContactApp userObject/src/UserApp/UserApp.jsx
+++ Practiced Backup files/ContactApp userObject/src/UserApp/UserApp.jsx
@@ -3,46 +3,46 @@ import UserDetails from './UserDetails'
 import UserList from './UserList'
 import Axios from "axios"
 class UserApp extends Component {
-constructor(props){
-    super(props);
-    this.state={
-        persons:{},
-        contacts:{}
+    constructor(props) {
+        super(props);
+        this.state = {
+            persons: {},
+            contacts: {}
+        }
     }
-}
 
-selUser = (user)=>{
-    console.log(user.firstName)
-    this.setState({contacts:user})
-}
+    selUser = (user) => {
+        console.log(user.firstName)
+        this.setState({ contacts: user })
+    }
 
-componentDidMount=()=>{
-Axios.get('https://dummyjson.com/users').then((resp)=>{
-    this.setState({persons:resp.data})
-}).catch()
-}
+    componentDidMount = () => {
+        Axios.get('https://dummyjson.com/users').then((resp) => {
+            this.setState({ persons: resp.data })
+        }).catch()
+    }
     render() {
 
 
         return <>
-        <h1>Contact App</h1>
-        <pre>{JSON.stringify(this.state)}</pre>
+            <h1>Contact App</h1>
+            <pre>{JSON.stringify(this.state)}</pre>
             <div className="container">
                 <div className="row">
                     <div className="col-md-8" id="UserList">
-                    <h1>Contact Details</h1>
-                    <UserList users={this.state.persons} userDetails={this.selUser} />                                       {/* for UserList */}
-                    
+                        <h1>Contact Details</h1>
+                        <UserList users={this.state.persons} userDetails={this.selUser} />                                       {/* for UserList */}
+
                     </div>
                     <div className="col-md-4" id="UserDetails" >
-                    <h1>Contact Details</h1>
+                        <h1>Contact Details</h1>
                         {Object.keys(this.state.contacts).length > 0 ?
-                         <>
-                        
-                         <UserDetails contact={this.state.contacts} /> 
-                         </> : null
-    }
-                                                                  {/* for UserDetails */}
+                            <>
+
+                                <UserDetails contact={this.state.contacts} />
+                            </> : null
+                        }
+                        {/* for UserDetails */}
                     </div>
                 </div>
             </div>
