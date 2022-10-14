939900a0c5bb24d637aed03b01394a3df02d5e0f
karkuvelpandi <karkuvelpandi007@gmail.com>
2022-10-14 20:43:12 +0530

    search action in product app

--- react-all/src/Navbar/Navbar.jsx
+++ react-all/src/Navbar/Navbar.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 let Navbar = () => {
-    return <nav className="navbar navbar-dark bg-dark navbar expand-lg">
+    return <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
         <Link to='' className='navbar-brand'>Hooks</Link>
         <ul className='navbar-nav ml-auto'>
-            <li className='nav-list'>  <Link to='UseState' className="nav-items">UseState</Link> </li>
-            <li className='nav-list'> <Link to='Counter' className="nav-items">Counter</Link> </li>
-            <li className='nav-list'>  <Link to='Clock' className="nav-items">Clock</Link> </li>
-            <li className='nav-list'>  <Link to='ContactApp' className="nav-items">ContactApp</Link></li>
-            <li className='nav-list'>  <Link to='UserApp' className="nav-items">UserApp</Link></li>
-            <li className='nav-list'>  <Link to='ProductApp' className="nav-items">ProductApp</Link></li>
+            <li className='nav-list'>  <Link to='UseState' className="nav-item">UseState</Link> </li>
+            <li className='nav-list'> <Link to='Counter' className="nav-item">Counter</Link> </li>
+            <li className='nav-list'>  <Link to='Clock' className="nav-item">Clock</Link> </li>
+            <li className='nav-list'>  <Link to='ContactApp' className="nav-item">ContactApp</Link></li>
+            <li className='nav-list'>  <Link to='UserApp' className="nav-item">UserApp</Link></li>
+            <li className='nav-list'>  <Link to='ProductApp' className="nav-item">ProductApp</Link></li>
         </ul>
     </nav>
 }
--- react-all/src/ProductApp/ProductList.jsx
+++ react-all/src/ProductApp/ProductList.jsx
@@ -1,16 +1,65 @@
 import React from 'react'
-
+import { useState } from 'react'
 let ProductList = (props) => {
 
-   let selectedUser=(user)=>{
-             console.log(user.id);
-            props.selprofile(user);
-   };
+    let selectedUser = (user) => {
+        console.log(user.id);
+        props.selprofile(user);
+    };
+
+    //Search Operation Part
+    const dataArray = props.userData.data.users
+    const [searchName, setSearchName] = useState("")
+    const [foundUser, setFoundUser] = useState(dataArray)
+
+    let getData = (e) => {
+        const keyword = e.target.value
+        console.log(keyword)
+        setSearchName(keyword)
+    };
+
+    let filter = () => {
+        if (searchName !== "") {
+            const result = foundUser.filter((singleData) => {
+
+                return singleData.firstName.toLowerCase().startsWith(searchName.toLowerCase())
+
+            })
+
+            setFoundUser(result)
+        }
+        if (searchName == "") {
+            setFoundUser(dataArray)
+        }
+    };
+    let refresh = () => {
+        const searchName = ""
+        if (searchName == "") {
+            setFoundUser(dataArray)
+        }
+    };
 
     return <>
         <div>Product List</div>
-        {/* <pre>{JSON.stringify(props)}</pre> */}
+        <pre>{JSON.stringify(props)}</pre>
         <div className="container">
+            <div className="row">
+
+                <input
+                    type="search"
+                    value={searchName}
+                    placeholder="Filter By Name"
+                    onChange={getData}      
+                />
+                <input type="button" value='search' onClick={filter.bind(this, searchName)} />
+                <input type="button" value='refresh' onClick={refresh.bind(this, searchName)} />
+
+
+            </div>
+        </div><br />
+
+        <div className="container">
+
             <div className="row">
                 <div className="col-md">
                     <table className='table table-hover'>
@@ -23,14 +72,20 @@ let ProductList = (props) => {
                             </tr>
                         </thead>
                         <tbody className='bg-light '>
-                          {props.userData.data.users.map((user)=>{
-                            return <tr key={user.id} onClick={selectedUser.bind(this,user)}>
-                                <td>{user.id}</td>
-                                <td>{user.firstName}</td>
-                                <td>{user.email}</td>
-                                <td>{user.address.city}</td>
-                            </tr>
-                          })}
+
+                            {foundUser.length > 0 ? (
+
+                                foundUser.map((user) => {
+                                    return <tr key={user.id} onClick={selectedUser.bind(this, user)}>
+                                        <td>{user.id}</td>
+                                        <td>{user.firstName}</td>
+                                        <td>{user.email}</td>
+                                        <td>{user.address.city}</td>
+                                    </tr>
+                                })
+                            ) : (
+                                <h1>No results found!</h1>
+                            )}
                         </tbody>
                     </table>
                 </div>
