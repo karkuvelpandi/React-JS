5f00cd47b8a41bbbc225132f2dacb038f9cf157c
karkuvelpandi <karkuvelpandi007@gmail.com>
2023-02-23 10:50:05 +0530

    ok

--- react-all/src/App.js
+++ react-all/src/App.js
@@ -6,12 +6,30 @@ import Clock from './Clock/Clock'
 import ContactApp from './ContactApp/ContactApp'
 import UserApp from "./UserApp/UserApp"
 import ProductApp from "./ProductApp/ProductApp"
-
 import {BrowserRouter as Router, Routes,Route}from 'react-router-dom'
+import React from 'react'
+import "./App.css"
+import ToDo from './ToDo/ToDo'
+import {Link, BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Pending from './ToDo/Pending'
+import Completed from './ToDo/Completed'
+import Book from './stateVSvariables/stateVSvariables'
+import HOCComp from './HOC Component/HOCComp'
+import UseMousePosition from './Cursor/UseMousePosition'
+import Download from './Download/Download.tsx'
+import Pagination from './Pagination/Pagination'
 let App =()=>{
    return <>
    <Router>
       <Navbar/>
+      <Book/>
+        <HOCComp />
+        <Download/>
+        <br/>
+        <hr/>
+        <br/>
+        <hr/>
+        <Pagination/>
       <Routes>
          <Route path='/UseState' element={<UseState/>}/>
          <Route path='/Counter' element={<Counter/>}/>
@@ -19,6 +37,9 @@ let App =()=>{
          <Route exact path='/ContactApp' element={<ContactApp/>}/>
          <Route exact path='/UserApp' element={<UserApp/>}/>
          <Route exact path='/ProductApp' element={<ProductApp/>}/>
+         <Route path="/completed" element={<Completed />} />
+        <Route path="/pending" element={<Pending />} />
+        <Route path="/todo" element={<ToDo />} /> 
       </Routes>
    </Router>
    
--- react-testing-app/src/App.js
+++ react-testing-app/src/App.js
@@ -1,37 +1,34 @@
-import React from 'react'
-import "./App.css"
-import ToDo from './ToDo/ToDo'
-import {Link, BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Pending from './ToDo/Pending'
-import Completed from './ToDo/Completed'
-import Book from './stateVSvariables/stateVSvariables'
-import HOCComp from './HOC Component/HOCComp'
-import UseMousePosition from './Cursor/UseMousePosition'
-import Download from './Download/Download.tsx'
-import Pagination from './Pagination/Pagination'
-let App = () => {
+import React, { useState } from 'react'
+
+const App = () => {
+  let[resultflag,setResultflag]=useState(null)
+  let[input,setInput]=useState(null)
+  let isPrime=(number)=>{
+      if(number<=1){
+        return false
+      }
+      for(let i=2;i<=Math.sqrt(number);i++){
+            if(number%i===0){
+              return false
+            }
+      }
+      return true
+  }
+  let inputNumber=()=>{
+     let input=prompt()
+     setInput(Number(input))
+     let flag=isPrime(input)
+     console.log(flag);
+     setResultflag(flag) 
+  }
   return <>
-    <nav className='navbar navbar-dark bg-light navbar-expand-lg'>
-      <a href='/todo'>ToDo App</a>
-       <a to='/todo'> main</a> 
-    </nav>
-    <Router>
-        <Book/>
-        <HOCComp />
-        <Download/>
-        <br/>
-        <hr/>
-        <br/>
-        <hr/>
-        <Pagination/>
-      <Routes>
-        
-        <Route path="/completed" element={<Completed />} />
-        <Route path="/pending" element={<Pending />} />
-        <Route path="/todo" element={<ToDo />} /> 
-      </Routes>
-    </Router>
-    
+  <div>
+    {/* <pre>{JSON.stringify(resultflag)}</pre> */}
+    <h1>{input}</h1>
+    <h3 style={resultflag?{color:"green"}:{color:"red"}}>{resultflag?"Yes its a prime number":"No its not a prime number"}</h3>
+    <button onClick={inputNumber}>Input</button>
+  </div>
   </>
 }
+
 export default App
